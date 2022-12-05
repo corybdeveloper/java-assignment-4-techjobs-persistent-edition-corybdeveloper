a68b118c50ab24529de9c1a0e1272c67993628a0
Rita Clark-Browning <ritaclark-browning@iMac-607.local>
2022-12-05 16:55:48 -0600

    Complete up to Part 3

--- queries.sql
+++ queries.sql
@@ -6,6 +6,9 @@
 
 -- Part 2: Test it with SQL
 
+-- SELECT * FROM techjobs.employer
+-- WHERE (location = "St. Louis City");
+
 -- Part 3: Test it with SQL
 
 -- Part 4: Test it with SQL
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,10 +1,14 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(max = 255)
     private String location;
 
     public Employer(String location) {
